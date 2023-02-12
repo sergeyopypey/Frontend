1e441e0f1e7151ed3ab3b20f43ff622094dcab1c
seregeyopypey <19.02.02@mail.ru>
2023-02-12 17:08:31 +0300

    SAP GI 3.1

--- sorting.js
+++ sorting.js
@@ -1,21 +1,18 @@
 import { getLicenses, insertLicense } from "./Controllers/licenseController";
 
-export async function sortByExpirationDate()
-{
-    console.log('sortByExpirationDate was called')
+export async function sortBySummary() {
+    console.log('sortBySummary was called')
     const json = await getLicenses()
     console.log(json)
     console.log(json[1])
+
 }
 
-export async function sortByOwner()
-{
+export async function sortByOwner() {
     console.log('sortByOwner was called')
 
 }
 
-export async function sortBySummary()
-{
-    console.log('sortBySummary was called')
-
+export async function sortByExpirationDate() {
+    console.log('sortByExpirationDate was called')
 }
